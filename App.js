863ef2239d71cab323c2677848722f31e6e9854a
marsellavaleria19 <marsellavaleria19@gmail.com>
2022-04-01 13:25:05 +0700

    feat : update layout and routing

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import Payment from './src/screen/payment/Payment';
 import {View} from 'react-native';
 import Login from './src/screen/auth/Login';
 import Signup from './src/screen/auth/Signup';
@@ -8,12 +8,26 @@ import {NavigationContainer} from '@react-navigation/native';
 import AuthNav from './src/components/AuthNavigator';
 import NavMainStack from './src/components/MainStack';
 import Reservation from './src/screen/Reservation';
+import {NativeBaseProvider, extendTheme} from 'native-base';
 
 const App = () => {
+   const theme = extendTheme({
+      components: {
+         Input: {
+            baseStyle: {
+               rounded: 'md',
+               backgroundColor: '#DFDEDE',
+            },
+         },
+      },
+   });
    return (
-      <NavigationContainer>
-         <NavMainStack />
-      </NavigationContainer>
+      <NativeBaseProvider theme={theme}>
+         {/* <NavigationContainer>
+            <NavMainStack />
+         </NavigationContainer> */}
+         <Payment />
+      </NativeBaseProvider>
    );
 };
 
