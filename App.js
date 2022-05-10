dde6bcfb8a019f082001fed33cd98d09a05181cb
marsellavaleria19 <marsellavaleria19@gmail.com>
2022-05-10 14:27:04 +0700

    feat : update app js

--- App.js
+++ App.js
@@ -1,17 +1,9 @@
-import React, {useEffect} from 'react';
-import Payment from './src/screen/payment/Payment';
-import {NativeBaseProvider, extendTheme} from 'native-base';
-import {input, button} from './src/assets/styles/styleComponent';
-import stylePrimary from './src/assets/styles/stylePrimary';
-import {NavigationContainer} from '@react-navigation/native';
-import NavMainStack from './src/components/MainStack';
-import History from './src/screen/History';
+import React from 'react';
 import Main from './src/screen/Main';
 import {Provider} from 'react-redux';
 import {PersistGate} from 'redux-persist/integration/react';
 import ReduxStore from './src/redux/store';
 import messaging from '@react-native-firebase/messaging';
-import PushNotification from 'react-native-push-notification';
 
 const App = () => {
    const {store, persistor} = ReduxStore();
