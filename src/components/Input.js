d09888346b0414d88e193f3ae1ff56d5483daee6
marsellavaleria19 <marsellavaleria19@gmail.com>
2022-05-13 13:57:05 +0700

    feat : add place holder color in input component

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import {Text, View, StyleSheet, TextInput} from 'react-native';
+import AntDesigns from 'react-native-vector-icons/AntDesign';
 
 const Input = ({
    placeholder,
@@ -8,21 +9,52 @@ const Input = ({
    secure,
    change,
    valueDefault,
+   placeholderTextColor,
    children,
+   error = null,
    keyboardType,
 }) => {
    return (
-      <TextInput
-         style={classInput}
-         placeholder={placeholder}
-         placeholderTextColor="white"
-         defaultValue={valueDefault}
-         value={value}
-         onChangeText={change}
-         secureTextEntry={secure}
-         keyboardType={keyboardType}
-      />
+      <>
+         <TextInput
+            style={classInput}
+            placeholder={placeholder}
+            placeholderTextColor={placeholderTextColor}
+            defaultValue={valueDefault}
+            value={value}
+            onChangeText={change}
+            secureTextEntry={secure}
+            keyboardType={keyboardType}
+         />
+         {error !== null && (
+            <View style={addStyles.layoutError}>
+               <AntDesigns
+                  name="exclamationcircleo"
+                  style={addStyles.warningIcon}
+               />
+               <Text style={addStyles.textError}>{error}</Text>
+            </View>
+         )}
+      </>
    );
 };
 
+const addStyles = StyleSheet.create({
+   layoutError: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      color: 'red',
+   },
+   textError: {
+      fontSize: 12,
+      color: 'red',
+      marginLeft: 3,
+   },
+   warningIcon: {
+      fontSize: 14,
+      color: '#f87171',
+   },
+});
+
+export {addStyles};
 export default Input;
